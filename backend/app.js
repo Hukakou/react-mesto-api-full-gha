1407f4c4acbf6bdd71353110c041e9cf93001163
Vladimir <derekpro@mail.ru>
2023-06-06 18:11:26 +0300

    15 sprint start test

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const { createUserJoi, loginJoi } = require('./middlewares/celebrate');
+const auth = require('./middlewares/auth');
+const router = require('./routes/router');
+const {
+  createUser,
+  login,
+} = require('./controllers/users');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const mongodbURL = 'mongodb://0.0.0.0:27017/mestodb';
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(express.json());
+app.use(requestLogger);
+app.post('/signin', loginJoi, login);
+app.post('/signup', createUserJoi, createUser);
+app.use(auth);
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+
+app.use((error, request, res, next) => {
+  const {
+    status = 500,
+    message,
+  } = error;
+  res.status(status)
+    .send({
+      message: status === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
+mongoose
+  .connect(mongodbURL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, (err) => {
+  // eslint-disable-next-line no-unused-expressions
+  err ? console.log(err) : console.log(`App listening on ${PORT}`);
+});
---
+++ backend/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Жак-Ив Кусто',
+  },
+  about: {
+    type: String,
+    required: false,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Исследователь',
+  },
+  avatar: {
+    type: String,
+    required: false,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Некорректная ссылка на аватар',
+    },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Некорректная электронная почта',
+    },
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
